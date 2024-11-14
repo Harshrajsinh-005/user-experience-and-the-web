4d22f6e633516ef0fac53564da31fcf8c469b3d9
hwamzz <72756585+hwamzz@users.noreply.github.com>
2024-11-14 13:57:08 +0000

    Add files via upload

--- main.js
+++ main.js
@@ -1,20 +1,9 @@
-window.addEventListener("load", () => {
-    const dialog =  document.getElementById("cookies")
-    const acceptButton = document.getElementById("acceptCookies")
-    const rejectButton = document.getElementById("rejectCookies")
-    let cookiesSet = dialog.className
-
-    if ((cookiesSet == "false")) {
-        dialog.showModal()
-    }
-
-    acceptButton.addEventListener("click", () => {
-        dialog.close()
-        dialog.className = "true"
-    })
-
-    rejectButton.addEventListener("click", () => {
-        dialog.close()
-        dialog.className = "true"
-    })
-})
+/*
+  - book title
+  - book author
+  - release date
+  - summary
+  - genre
+  - pictures
+  - coordinates of author 
+*/
